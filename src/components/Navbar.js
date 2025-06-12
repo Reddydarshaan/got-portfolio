423c583bc9b67ed59a040f5d3a09c9d6a78bf031
Reddydarshaan <reddydarshaan@gmail.com>
2025-06-12 22:22:28 +0530

    Restored original Navbar.js and fixed href attribute

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,26 +3,37 @@ import '../styles/Navbar.css';
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar-brand">
-        <a href="/" className="navbar-title">
-          Game of Thrones Portfolio
-        </a>
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <div className="container">
+        <a className="navbar-brand text-gold" href="/">Reddy Darshaan.D</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#about">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#projects">Projects</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#skills">Skills</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <ul className="navbar-links">
-        <li>
-          <a href="#home" className="navbar-link">Home</a>
-        </li>
-        <li>
-          <a href="#about" className="navbar-link">About</a>
-        </li>
-        <li>
-          <a href="#projects" className="navbar-link">Projects</a>
-        </li>
-        <li>
-          <a href="#contact" className="navbar-link">Contact</a>
-        </li>
-      </ul>
     </nav>
   );
 };
